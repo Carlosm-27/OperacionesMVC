47889a3fe455e3f0bd0aa63643858a42c0387ac2
Carlosm-27 <71892185+Carlosm-27@users.noreply.github.com>
2020-11-15 18:59:15 -0600

    Add project files.

---
+++ OperacionesMVC/Controllers/OperacionesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OperacionesMVC.Models;
+
+namespace OperacionesMVC.Controllers
+{
+    public class OperacionesController : Controller
+    {
+        // GET: Operaciones
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Suma()
+        {
+
+            Datos objModelo = new Datos();
+            objModelo.variableA = 3;
+            objModelo.variableB = 5;
+            objModelo.resultado = objModelo.variableA + objModelo.variableB;
+            ViewData["Resultado"] = objModelo.resultado;
+            return View();
+        }
+
+        public ActionResult Resta()
+        {
+
+            Datos objModelo = new Datos();
+
+            try
+            {
+                objModelo.variableA = Convert.ToDouble(Request.Form["txtvalor1"].ToString());
+                objModelo.variableB = Convert.ToDouble(Request.Form["txtvalor2"].ToString());
+                objModelo.resultado = objModelo.variableA - objModelo.variableB;
+            }
+            catch{ 
+            
+            }
+            return View("Resta", objModelo);
+        }
+
+
+        public ActionResult Division()
+        {
+
+            Datos objModelo = new Datos();
+            objModelo.variableA = 100;
+            objModelo.variableB = 80;
+            objModelo.resultado = objModelo.variableA / objModelo.variableB;
+            ViewData["Resultado"] = objModelo.resultado;
+            return View();
+        }
+
+
+        public ActionResult Multiplicacion()
+        {
+
+            Datos objModelo = new Datos();
+
+            try
+            {
+                objModelo.variableA = Convert.ToDouble(Request.Form["txtvalor1"].ToString());
+                objModelo.variableB = Convert.ToDouble(Request.Form["txtvalor2"].ToString());
+                objModelo.resultado = objModelo.variableA * objModelo.variableB;
+            }
+            catch
+            {
+
+            }
+            return View("Multiplicacion", objModelo);
+        }
+    }
+}
---
+++ OperacionesMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OperacionesMVC.MvcApplication" Language="C#" %>
